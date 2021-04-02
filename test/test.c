8ced03fec44737ca7656cb04844246649337cd87
devcon <devcon@admantium.com>
2021-04-02 17:35:23 +0200

    test write bitmask

--- test/test.c
+++ test/test.c
@@ -15,6 +15,7 @@
 #include <cmocka.h>
 
 typedef bool bit;
+typedef u_int8_t bitmask;
 
 typedef struct
 {
@@ -22,13 +23,25 @@ typedef struct
   int SHIFT_REGISTER_CLOCK_PIN;
   int LATCH_REGISTER_CLOCK_PIN;
 
-  bool serial_pin_state;
+  bit serial_pin_state;
+  bitmask register_state;
 } ShiftRegister;
 
 bool write_bit(bit b, ShiftRegister *reg)
 {
+  printf("Write Bit %d\n", b);
   reg->serial_pin_state = b;
   return b;
+}
+
+bool write_bitmask(bitmask btm, ShiftRegister *reg)
+{
+  int b = 0b0000001;
+  while (b <= 128)
+  {
+    ((b <<= 1) & btm) ? write_bit(1, reg) : write_bit(0, reg);
+  }
+  return true;
 }
 
 void test_shift_register_config(void **state)
@@ -48,6 +61,13 @@ void test_write_bit(void **state)
   assert_int_equal(reg.serial_pin_state, 1);
 }
 
+void test_write_bitmask(void **state)
+{
+  ShiftRegister reg = {14, 11, 12};
+  write_bitmask(0b00010000, &reg);
+  assert_int_equal(reg.serial_pin_state, 1);
+}
+
 int main(int argc, char *argv[])
 {
   printf("Runing Tests\n");
@@ -57,6 +77,7 @@ int main(int argc, char *argv[])
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_shift_register_config),
       cmocka_unit_test(test_write_bit),
+      cmocka_unit_test(test_write_bitmask),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
