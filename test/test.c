78733ce3b3a0fb6c3d49471e9a570c361cd87c33
devcon <devcon@admantium.com>
2021-04-03 10:15:04 +0200

    reset_shift_register, reset_storage_register, test shift register content

--- test/test.c
+++ test/test.c
@@ -31,27 +31,43 @@ typedef struct
 
 bool write_bit(bit b, ShiftRegister *reg)
 {
-  printf("Write Bit %d\n", b);
+  // printf("Write Bit %d\n", b);
   reg->serial_pin_state = b;
+  (b) ? (reg->register_state += 0b10) : (reg->register_state += 0b00);
   return b;
 }
 
 bool write_bitmask(bitmask btm, ShiftRegister *reg)
 {
-  reg->register_state = btm;
   int b = 0b0000001;
   while (b <= 128)
   {
     ((b <<= 1) & btm) ? write_bit(1, reg) : write_bit(0, reg);
   }
+  reg->register_state = btm;
+  return true;
+}
+
+bool flush_shift_register(ShiftRegister *reg)
+{
+  return true;
+}
+
+bool reset_shift_register(ShiftRegister *reg)
+{
+  return write_bitmask(0b0000000, reg);
+}
+
+bool reset_strorage_register(ShiftRegister *reg)
+{
+  write_bitmask(0b0000000, reg);
+  flush_shift_register(reg);
   return true;
 }
 
 char *print_shift_register(ShiftRegister *reg)
 {
-  char *btm;
-  btm = (char *)malloc(sizeof(bit) * 8);
-
+  char *btm = (char *)malloc(sizeof(bit) * 8);
   int b = 0b00000001;
 
   for (int i = 0; i < 8; i++)
@@ -65,8 +81,6 @@ char *print_shift_register(ShiftRegister *reg)
       *(btm + i) = '0';
     }
   }
-  printf("Shift Register %s\n", btm);
-
   return btm;
 }
 
@@ -85,6 +99,9 @@ void test_write_bit(void **state)
   assert_int_equal(reg.serial_pin_state, 0);
   write_bit(1, &reg);
   assert_int_equal(reg.serial_pin_state, 1);
+
+  printf("Shift Register: %s\n", print_shift_register(&reg));
+  assert_memory_equal(print_shift_register(&reg), &"10000000", 8);
 }
 
 void test_write_bitmask(void **state)
@@ -92,6 +109,18 @@ void test_write_bitmask(void **state)
   ShiftRegister reg = {14, 11, 12};
   write_bitmask(0b00010000, &reg);
   assert_int_equal(reg.serial_pin_state, 0);
+
+  printf("Shift Register: %s\n", print_shift_register(&reg));
+  assert_memory_equal(print_shift_register(&reg), &"00010000", 8);
+}
+
+void test_reset_shift_register(void **state)
+{
+  ShiftRegister reg = {14, 11, 12};
+  write_bitmask(0b01110001, &reg);
+  reset_shift_register(&reg);
+
+  assert_memory_equal(print_shift_register(&reg), &"00000000", 8);
   print_shift_register(&reg);
 }
 
@@ -105,6 +134,7 @@ int main(int argc, char *argv[])
       cmocka_unit_test(test_shift_register_config),
       cmocka_unit_test(test_write_bit),
       cmocka_unit_test(test_write_bitmask),
+      cmocka_unit_test(test_reset_shift_register),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
