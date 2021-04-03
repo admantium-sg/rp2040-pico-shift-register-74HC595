dd4463968c3761d432f1d4db499db1d8d1411b36
devcon <devcon@admantium.com>
2021-04-03 09:17:33 +0200

    print shift register

--- test/test.c
+++ test/test.c
@@ -9,6 +9,8 @@
 */
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
@@ -19,9 +21,9 @@ typedef u_int8_t bitmask;
 
 typedef struct
 {
-  int SERIAL_PIN;
-  int SHIFT_REGISTER_CLOCK_PIN;
-  int LATCH_REGISTER_CLOCK_PIN;
+  u_int8_t SERIAL_PIN;
+  u_int8_t SHIFT_REGISTER_CLOCK_PIN;
+  u_int8_t LATCH_REGISTER_CLOCK_PIN;
 
   bit serial_pin_state;
   bitmask register_state;
@@ -36,12 +38,36 @@ bool write_bit(bit b, ShiftRegister *reg)
 
 bool write_bitmask(bitmask btm, ShiftRegister *reg)
 {
+  reg->register_state = btm;
   int b = 0b0000001;
   while (b <= 128)
   {
     ((b <<= 1) & btm) ? write_bit(1, reg) : write_bit(0, reg);
   }
   return true;
+}
+
+char *print_shift_register(ShiftRegister *reg)
+{
+  char *btm;
+  btm = (char *)malloc(sizeof(bit) * 8);
+
+  int b = 0b00000001;
+
+  for (int i = 0; i < 8; i++)
+  {
+    if ((b <<= 1) & reg->register_state)
+    {
+      *(btm + i) = '1';
+    }
+    else
+    {
+      *(btm + i) = '0';
+    }
+  }
+  printf("Shift Register %s\n", btm);
+
+  return btm;
 }
 
 void test_shift_register_config(void **state)
@@ -65,7 +91,8 @@ void test_write_bitmask(void **state)
 {
   ShiftRegister reg = {14, 11, 12};
   write_bitmask(0b00010000, &reg);
-  assert_int_equal(reg.serial_pin_state, 1);
+  assert_int_equal(reg.serial_pin_state, 0);
+  print_shift_register(&reg);
 }
 
 int main(int argc, char *argv[])
