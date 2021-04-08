1319d17a3a84a747d90ea36c2db75a2f26620a56
devcon <devcon@admantium.com>
2021-04-08 21:05:14 +0200

    update: Change mocks to .h file

---
+++ test/mocks.h
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#define GPIO_OUT 1
+
+void stdio_init_all()
+{
+  //do nothing
+}
+
+void gpio_init(uint8_t gpio)
+{
+  //do nothing
+}
+
+static void gpio_set_dir(uint8_t gpio, bool out)
+{
+  //do nothing
+}
+
+static void gpio_put(uint8_t gpio, bool value)
+{
+  //do nothing
+}
